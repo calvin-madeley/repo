0ce310b8174be4c0991f43afeedba18582affdf4
Calvin Madeley <Calvin.Madeley@outlook.com>
2025-02-13 08:58:14 +0800

    <chore> updates sql with double quotes for field/schema/db names

--- DATA_EXPLORATION.sql
+++ DATA_EXPLORATION.sql
@@ -1,19 +1,19 @@
 
 -- Set the current database to RECRUITMENT_DB
-USE DATABASE RECRUITMENT_DB;
+USE DATABASE "RECRUITMENT_DB";
 
 -- Set the current schema to candidate_00139
-USE SCHEMA candidate_00139;
+USE SCHEMA "candidate_00139";
 
 -- Create or replace a transient table to hold raw flight data
-CREATE OR REPLACE TRANSIENT TABLE FLIGHTS_RAW (TXT VARCHAR);
+CREATE OR REPLACE TRANSIENT TABLE "FLIGHTS_RAW" (TXT VARCHAR);
 
 -- Load data into the FLIGHTS_RAW table from a gzip compressed CSV file
-COPY INTO FLIGHTS_RAW
+COPY INTO "FLIGHTS_RAW"
 FROM @RECRUITMENT_DB.PUBLIC.S3_FOLDER/flights.gz
 FILE_FORMAT = (TYPE = 'CSV', 
                FIELD_OPTIONALLY_ENCLOSED_BY = '"', 
                COMPRESSION = 'GZIP');
 
 -- Select the first 2 rows from FLIGHTS_RAW to determine schema from header information
-SELECT * FROM FLIGHTS_RAW LIMIT 2;
+SELECT * FROM "FLIGHTS_RAW" LIMIT 2;
--- FLIGHT_DIM_MODEL.sql
+++ FLIGHT_DIM_MODEL.sql
@@ -1,11 +1,11 @@
 -- Set the current database to RECRUITMENT_DB
-USE DATABASE RECRUITMENT_DB;
+USE DATABASE "RECRUITMENT_DB";
 
 -- Set the current schema to candidate_00139
-USE SCHEMA candidate_00139;
+USE SCHEMA "candidate_00139";
 
 -- Create or replace a table to store airport information
-CREATE OR REPLACE TABLE AIRPORT_DIM (
+CREATE OR REPLACE TABLE "AIRPORT_DIM" (
         "AIRPORTCODE" VARCHAR(250) NOT NULL PRIMARY KEY
         , "AIRPORTNAME" VARCHAR(250)
         , "CITYNAME" VARCHAR(250)
@@ -14,57 +14,57 @@ CREATE OR REPLACE TABLE AIRPORT_DIM (
     ) AS (
         WITH DEST_CTE AS (
             SELECT
-                ORIGINAIRPORTCODE AS AIRPORTCODE,
-                TRIM(SPLIT_PART(ORIGAIRPORTNAME, ':', 2)) AS AIRPORTNAME,
-                ORIGINCITYNAME AS CITYNAME,
-                ORIGINSTATE AS STATE,
-                ORIGINSTATENAME AS STATENAME
+                "ORIGINAIRPORTCODE" AS "AIRPORTCODE",
+                TRIM(SPLIT_PART("ORIGAIRPORTNAME", ':', 2)) AS "AIRPORTNAME",
+                "ORIGINCITYNAME" AS "CITYNAME",
+                "ORIGINSTATE" AS "STATE",
+                "ORIGINSTATENAME" AS "STATENAME"
             FROM
-                FLIGHT_RAW
+                "FLIGHT_RAW"
             GROUP BY
-                ORIGINAIRPORTCODE, ORIGAIRPORTNAME, ORIGINCITYNAME, ORIGINSTATE, ORIGINSTATENAME
+                "ORIGINAIRPORTCODE", "ORIGAIRPORTNAME", "ORIGINCITYNAME", "ORIGINSTATE", "ORIGINSTATENAME"
         ), ORIG_CTE AS (
             SELECT
-                DESTAIRPORTCODE AS AIRPORTCODE,
-                TRIM(SPLIT_PART(DESTAIRPORTNAME, ':', 2)) AS AIRPORTNAME,
-                DESTCITYNAME AS CITYNAME,
-                DESTSTATE AS STATE,
-                DESTSTATENAME AS STATENAME
+                "DESTAIRPORTCODE" AS "AIRPORTCODE",
+                TRIM(SPLIT_PART("DESTAIRPORTNAME", ':', 2)) AS "AIRPORTNAME",
+                "DESTCITYNAME" AS "CITYNAME",
+                "DESTSTATE" AS "STATE",
+                "DESTSTATENAME" AS "STATENAME"
             FROM
-                FLIGHT_RAW
+                "FLIGHT_RAW"
             GROUP BY
-                DESTAIRPORTCODE, DESTAIRPORTNAME, DESTCITYNAME, DESTSTATE, DESTSTATENAME
+                "DESTAIRPORTCODE", "DESTAIRPORTNAME", "DESTCITYNAME", "DESTSTATE", "DESTSTATENAME"
         )
         SELECT
-            AIRPORTCODE,
-            AIRPORTNAME,
-            CITYNAME,
-            STATE,
-            STATENAME
+            "AIRPORTCODE",
+            "AIRPORTNAME",
+            "CITYNAME",
+            "STATE",
+            "STATENAME"
         FROM (
             SELECT * FROM ORIG_CTE
             UNION ALL 
             SELECT * FROM DEST_CTE
         )
         GROUP BY
-            AIRPORTCODE, AIRPORTNAME, CITYNAME, STATE, STATENAME
+            "AIRPORTCODE", "AIRPORTNAME", "CITYNAME", "STATE", "STATENAME"
     );
 
 -- Check for duplicate AIRPORTCODE in AIRPORT_DIM
-SELECT COUNT(1) AS QTY, AIRPORTCODE FROM AIRPORT_DIM GROUP BY AIRPORTCODE ORDER BY 1 DESC;
+SELECT COUNT(1) AS QTY, "AIRPORTCODE" FROM "AIRPORT_DIM" GROUP BY "AIRPORTCODE" ORDER BY 1 DESC;
 
 -- Create or replace a table to store distinct airline information
-CREATE OR REPLACE TABLE AIRLINE_DIM (
+CREATE OR REPLACE TABLE "AIRLINE_DIM" (
     "AIRLINECODE" VARCHAR(250) NOT NULL PRIMARY KEY
     , "AIRLINENAME" VARCHAR(250)
     , "AIRLINECOMMENTS" VARCHAR(250)
 ) AS (
     SELECT DISTINCT
-        AIRLINECODE,
-        TRIM(SPLIT_PART(AIRLINENAME, ':', 1)) AS AIRLINENAME,
-        TRIM(REPLACE(TRIM(SPLIT_PART(AIRLINENAME, ':', 2)), AIRLINECODE)) AS AIRLINECOMMENTS
+        FR."AIRLINECODE",
+        TRIM(SPLIT_PART(FR."AIRLINENAME", ':', 1)) AS "AIRLINENAME",
+        TRIM(REPLACE(TRIM(SPLIT_PART(FR."AIRLINENAME", ':', 2)), FR."AIRLINECODE")) AS AIRLINECOMMENTS
     FROM
-        FLIGHT_RAW
+        "FLIGHT_RAW" FR
 );
 
 CREATE OR REPLACE TABLE DATE_DIM (
@@ -86,28 +86,28 @@ CREATE OR REPLACE TABLE DATE_DIM (
         WHERE MY_DATE < CURRENT_DATE
     )
     SELECT 
-        TO_CHAR(MY_DATE, 'YYYY-MM-DD') AS FLIGHTDATE,
-        MY_DATE AS DAY_DATE,
-        YEAR(MY_DATE) AS YEAR,
-        MONTH(MY_DATE) AS MONTH,
-        LEFT(MONTHNAME(MY_DATE), 3) AS MONTH_NAME,
-        DAY(MY_DATE) AS DAY_OF_MON,
-        DAYNAME(MY_DATE) AS DAY_OF_WEEK,
-        WEEKOFYEAR(MY_DATE) AS WEEK_OF_YEAR,
-        DAYOFYEAR(MY_DATE) AS DAY_OF_YEAR
+        TO_CHAR(MY_DATE, 'YYYY-MM-DD') AS "FLIGHTDATE",
+        MY_DATE AS "DAY_DATE",
+        YEAR(MY_DATE) AS "YEAR",
+        MONTH(MY_DATE) AS "MONTH",
+        LEFT(MONTHNAME(MY_DATE), 3) AS "MONTH_NAME",
+        DAY(MY_DATE) AS "DAY_OF_MON",
+        DAYNAME(MY_DATE) AS "DAY_OF_WEEK",
+        WEEKOFYEAR(MY_DATE) AS "WEEK_OF_YEAR",
+        DAYOFYEAR(MY_DATE) AS "DAY_OF_YEAR"
     FROM DATE_CTE);
 
-select * from DATE_DIM;
+select * from "DATE_DIM";
 
 -- Create a fact table to store flight information
-CREATE OR REPLACE TABLE FLIGHT_FACT (
+CREATE OR REPLACE TABLE "FLIGHT_FACT" (
     "TRANSACTIONID" VARCHAR(250) NOT NULL PRIMARY KEY,
-    "FLIGHTDATE" CHAR(10) NOT NULL REFERENCES DATE_DIM(FLIGHTDATE),
-    "AIRLINECODE" VARCHAR(250) NOT NULL REFERENCES AIRLINE_DIM(AIRLINECODE),
+    "FLIGHTDATE" CHAR(10) NOT NULL REFERENCES "DATE_DIM"("FLIGHTDATE"),
+    "AIRLINECODE" VARCHAR(250) NOT NULL REFERENCES "AIRLINE_DIM"("AIRLINECODE"),
     "TAILNUM" VARCHAR(250),
     "FLIGHTNUM" VARCHAR(250),
-    "ORIGINAIRPORTCODE" VARCHAR(250) NOT NULL REFERENCES AIRPORT_DIM(AIRPORTCODE),
-    "DESTAIRPORTCODE" VARCHAR(250) NOT NULL REFERENCES AIRPORT_DIM(AIRPORTCODE),
+    "ORIGINAIRPORTCODE" VARCHAR(250) NOT NULL REFERENCES "AIRPORT_DIM"("AIRPORTCODE"),
+    "DESTAIRPORTCODE" VARCHAR(250) NOT NULL REFERENCES "AIRPORT_DIM"("AIRPORTCODE"),
     "CRSDEPTIME" TIME(9),
     "DEPTIME" TIME(9),
     "DEPDELAY" NUMBER(38,0),
@@ -129,78 +129,78 @@ CREATE OR REPLACE TABLE FLIGHT_FACT (
 ) AS (
     WITH FLIGHT_PROC AS (
         SELECT
-            TRANSACTIONID,
-            FLIGHTDATE,
-            AIRLINECODE,
-            CASE WHEN TAILNUM LIKE 'UNKNOW%' THEN NULL ELSE TRIM(TAILNUM, '@ -') END AS TAILNUM, -- If TAILNUM starts with 'UNKNOW', set it to NULL, otherwise trim '@', spaces, and '-'
-            FLIGHTNUM,
-            ORIGINAIRPORTCODE,
-            DESTAIRPORTCODE,
-            COALESCE(TRY_TO_TIME(LPAD(CRSDEPTIME, 4, '0') || '00','HH24MISS'), CASE WHEN CRSDEPTIME = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS CRSDEPTIME, -- Convert CRSDEPTIME to TIME type, pad with zeros, set '2400' to '00:00:00'
-            COALESCE(TRY_TO_TIME(LPAD(DEPTIME, 4, '0') || '00','HH24MISS'), CASE WHEN DEPTIME = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS DEPTIME, -- Convert DEPTIME to TIME type, pad with zeros, set '2400' to '00:00:00'
-            DEPDELAY,
-            TAXIOUT,
-            COALESCE(TRY_TO_TIME(LPAD(WHEELSOFF, 4, '0') || '00','HH24MISS'), CASE WHEN WHEELSOFF = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS WHEELSOFF, -- Convert WHEELSOFF to TIME type, pad with zeros, set '2400' to '00:00:00'
-            COALESCE(TRY_TO_TIME(LPAD(WHEELSON, 4, '0') || '00','HH24MISS'), CASE WHEN WHEELSON = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS WHEELSON, -- Convert WHEELSON to TIME type, pad with zeros, set '2400' to '00:00:00'
-            TAXIIN,
-            COALESCE(TRY_TO_TIME(LPAD(CRSARRTIME, 4, '0') || '00','HH24MISS'), CASE WHEN CRSARRTIME = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS CRSARRTIME, -- Convert CRSARRTIME to TIME type, pad with zeros, set '2400' to '00:00:00'
-            COALESCE(TRY_TO_TIME(LPAD(ARRTIME, 4, '0') || '00','HH24MISS'), CASE WHEN ARRTIME = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS ARRTIME, -- Convert ARRTIME to TIME type, pad with zeros, set '2400' to '00:00:00'
-            ARRDELAY,
-            TRY_CAST(CRSELAPSEDTIME AS NUMBER) AS CRSELAPSEDTIME, -- Convert CRSELAPSEDTIME to NUMBER type
-            TRY_CAST(ACTUALELAPSEDTIME AS NUMBER) AS ACTUALELAPSEDTIME, -- Convert ACTUALELAPSEDTIME to NUMBER type
+            "TRANSACTIONID",
+            "FLIGHTDATE",
+            "AIRLINECODE",
+            CASE WHEN "TAILNUM" LIKE 'UNKNOW%' THEN NULL ELSE TRIM("TAILNUM", '@ -') END AS "TAILNUM", -- If TAILNUM starts with 'UNKNOW', set it to NULL, otherwise trim '@', spaces, and '-'
+            "FLIGHTNUM",
+            "ORIGINAIRPORTCODE",
+            "DESTAIRPORTCODE",
+            COALESCE(TRY_TO_TIME(LPAD("CRSDEPTIME", 4, '0') || '00','HH24MISS'), CASE WHEN "CRSDEPTIME" = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS "CRSDEPTIME", -- Convert CRSDEPTIME to TIME type, pad with zeros, set '2400' to '00:00:00'
+            COALESCE(TRY_TO_TIME(LPAD("DEPTIME", 4, '0') || '00','HH24MISS'), CASE WHEN "DEPTIME" = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS "DEPTIME", -- Convert DEPTIME to TIME type, pad with zeros, set '2400' to '00:00:00'
+            "DEPDELAY",
+            "TAXIOUT",
+            COALESCE(TRY_TO_TIME(LPAD("WHEELSOFF", 4, '0') || '00','HH24MISS'), CASE WHEN "WHEELSOFF" = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS "WHEELSOFF", -- Convert WHEELSOFF to TIME type, pad with zeros, set '2400' to '00:00:00'
+            COALESCE(TRY_TO_TIME(LPAD("WHEELSON", 4, '0') || '00','HH24MISS'), CASE WHEN "WHEELSON" = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS "WHEELSON", -- Convert WHEELSON to TIME type, pad with zeros, set '2400' to '00:00:00'
+            "TAXIIN",
+            COALESCE(TRY_TO_TIME(LPAD("CRSARRTIME", 4, '0') || '00','HH24MISS'), CASE WHEN "CRSARRTIME" = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS "CRSARRTIME", -- Convert CRSARRTIME to TIME type, pad with zeros, set '2400' to '00:00:00'
+            COALESCE(TRY_TO_TIME(LPAD("ARRTIME", 4, '0') || '00','HH24MISS'), CASE WHEN "ARRTIME" = '2400' THEN '00:00:00'::TIME ELSE NULL END) AS "ARRTIME", -- Convert ARRTIME to TIME type, pad with zeros, set '2400' to '00:00:00'
+            "ARRDELAY",
+            TRY_CAST("CRSELAPSEDTIME" AS NUMBER) AS "CRSELAPSEDTIME", -- Convert CRSELAPSEDTIME to NUMBER type
+            TRY_CAST("ACTUALELAPSEDTIME" AS NUMBER) AS "ACTUALELAPSEDTIME", -- Convert ACTUALELAPSEDTIME to NUMBER type
             CASE 
-                WHEN UPPER(CANCELLED) IN ('1','T','TRUE') THEN 1
-                WHEN UPPER(CANCELLED) IN ('0','F','FALSE') THEN 0
+                WHEN UPPER("CANCELLED") IN ('1','T','TRUE') THEN 1
+                WHEN UPPER("CANCELLED") IN ('0','F','FALSE') THEN 0
                 ELSE NULL
-            END AS CANCELLED, -- Convert CANCELLED to binary (1 or 0)
+            END AS "CANCELLED", -- Convert CANCELLED to binary (1 or 0)
             CASE 
-                WHEN UPPER(DIVERTED) IN ('1','T','TRUE') THEN 1
-                WHEN UPPER(DIVERTED) IN ('0','F','FALSE') THEN 0
+                WHEN UPPER("DIVERTED") IN ('1','T','TRUE') THEN 1
+                WHEN UPPER("DIVERTED") IN ('0','F','FALSE') THEN 0
                 ELSE NULL
-            END AS DIVERTED, -- Convert DIVERTED to binary (1 or 0)
-            DISTANCE,
-            TRY_CAST(TRIM(SPLIT_PART(DISTANCE, ' ', 1)) AS NUMBER) AS MILESDISTANCE -- Convert DISTANCE to numeric value
+            END AS "DIVERTED", -- Convert DIVERTED to binary (1 or 0)
+            "DISTANCE",
+            TRY_CAST(TRIM(SPLIT_PART("DISTANCE", ' ', 1)) AS NUMBER) AS "MILESDISTANCE" -- Convert DISTANCE to numeric value
         FROM
-            FLIGHT_RAW
+            "FLIGHT_RAW"
     )
     SELECT
-        TRANSACTIONID,
-        FLIGHTDATE,
-        AIRLINECODE,
-        TAILNUM,
-        FLIGHTNUM,
-        ORIGINAIRPORTCODE,
-        DESTAIRPORTCODE,
-        CRSDEPTIME,
-        DEPTIME,
-        DEPDELAY,
-        CASE WHEN DEPDELAY > 15 THEN 1 ELSE 0 END AS DEPDELAYGT15, -- Create binary field to indicate if departure delay is greater than 15 minutes
-        TAXIOUT,
-        WHEELSOFF,
-        WHEELSON,
-        TAXIIN,
-        CRSARRTIME,
-        ARRTIME,
-        ARRDELAY,
-        CRSELAPSEDTIME,
-        ACTUALELAPSEDTIME,
+        "TRANSACTIONID",
+        "FLIGHTDATE",
+        "AIRLINECODE",
+        "TAILNUM",
+        "FLIGHTNUM",
+        "ORIGINAIRPORTCODE",
+        "DESTAIRPORTCODE",
+        "CRSDEPTIME",
+        "DEPTIME",
+        "DEPDELAY",
+        CASE WHEN "DEPDELAY" > 15 THEN 1 ELSE 0 END AS "DEPDELAYGT15", -- Create binary field to indicate if departure delay is greater than 15 minutes
+        "TAXIOUT",
+        "WHEELSOFF",
+        "WHEELSON",
+        "TAXIIN",
+        "CRSARRTIME",
+        "ARRTIME",
+        "ARRDELAY",
+        "CRSELAPSEDTIME",
+        "ACTUALELAPSEDTIME",
         CASE 
-            WHEN ACTUALELAPSEDTIME IS NULL THEN 0
-            WHEN ACTUALELAPSEDTIME >= 1440 THEN 1
-            WHEN ARRTIME > DEPTIME THEN 0
+            WHEN "ACTUALELAPSEDTIME" IS NULL THEN 0
+            WHEN "ACTUALELAPSEDTIME" >= 1440 THEN 1
+            WHEN "ARRTIME" > "DEPTIME" THEN 0
             ELSE 1
-        END AS NEXTDAYARR, -- Create binary field to indicate if flight arrived the next day
-        CANCELLED,
-        DIVERTED,
-        DISTANCE,
+        END AS "NEXTDAYARR", -- Create binary field to indicate if flight arrived the next day
+        "CANCELLED",
+        "DIVERTED",
+        "DISTANCE",
         --MILESDISTANCE,
         CASE  
-            WHEN TRY_CAST(TRIM(SPLIT_PART(DISTANCE, ' ', 1)) AS NUMBER) <= 100 THEN '0-100 miles'
-            ELSE TO_VARCHAR(101 + (FLOOR((TRY_CAST(TRIM(SPLIT_PART(DISTANCE, ' ', 1)) AS NUMBER) - 101) / 100) * 100)) 
+            WHEN TRY_CAST(TRIM(SPLIT_PART("DISTANCE", ' ', 1)) AS NUMBER) <= 100 THEN '0-100 miles'
+            ELSE TO_VARCHAR(101 + (FLOOR((TRY_CAST(TRIM(SPLIT_PART("DISTANCE", ' ', 1)) AS NUMBER) - 101) / 100) * 100)) 
                     || '-' || 
-                    TO_VARCHAR(101 + (FLOOR((TRY_CAST(TRIM(SPLIT_PART(DISTANCE, ' ', 1)) AS NUMBER) - 101) / 100) * 100) + 99) 
+                    TO_VARCHAR(101 + (FLOOR((TRY_CAST(TRIM(SPLIT_PART("DISTANCE", ' ', 1)) AS NUMBER) - 101) / 100) * 100) + 99) 
                     || ' miles'
-        END AS DISTANCEGROUP -- Categorize distances into groups
+        END AS "DISTANCEGROUP" -- Categorize distances into groups
     FROM
         FLIGHT_PROC
 );
